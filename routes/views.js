f9163ce94d05cb8c5de51dc340ca079872ac8ad8
efexng <efeapoki2020@gmail.com>
2023-06-23 01:58:57 +0100

    ok

--- routes/views.js
+++ routes/views.js
@@ -1,16 +1,21 @@
-router.get('/authors', async (req, res) => {
-    let searchOptions = {};
-    if (req.query.name && req.query.name !== '') {
-        searchOptions.name = new RegExp(req.query.name, 'i');
+router.get('/', async (req, res) => {
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
-    try {
-        const authors = await Author.find(searchOptions).exec();
-        res.render('authors/index', {
-            authors: authors,
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try{
+        const books = await query.exec()
+        res.render('books/index', {
+            books: books,
             searchOptions: req.query
-        });
+        })
     } catch {
-        res.redirect('/');
+        res.redirect('/')
     }
 });
-
